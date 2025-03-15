efc6ab96ddef8b30eef08afb2f175b8c0348441f
kmer1024 <154684022+kmer1024@users.noreply.github.com>
2025-03-15 17:21:23 +0900

    Feat migration (#14)
    * Temporary Save
    
    * Feat: Migration 중간단계: Workspace까지 완료
    
    * Feat: 버블 삭제, 복구 구현
    
    * Chore: Path Validate 체크 transformer -> validator
    
    * Feat: getBubble API 구현
    
    * Feat: Bubble API 구현 완료
    
    * feat: Curve API migrate
    
    * Feat: fileAPI 구현
    
    * feat: migration end
    
    * api 수정
    
    * Migration Fix
    
    * Feat: CI/CD
    
    * Hotfix: CI/CD 수정
    
    * CICD 에러 수정
    
    * CICD error fix: Permission Denied
    
    * Chore: Dependency 삭제
    
    * Chore: docker file에 prisma 추가
    
    * CICD Testing
    
    * CHORE: CORS Domain Add
    
    ---------
    
    Co-authored-by: 이진혁 <jinhyeok@jinhyeok-ui-MacBookPro.local>

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:5173'],
+    origin: ['http://localhost:5173', 'https://app.mongle.xyz'],
     credentials: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type, Authorization',
